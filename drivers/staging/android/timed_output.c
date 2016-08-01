b1c92524245b2c63b41e11a6249c3da234cbf8f6
andip71 <andreasp@gmx.de>
2016-08-01 16:11:42 +0200

    synaptics_driver_s3320: Add sweep2wake gesture (OOS version)
    	Sysfs:
    
    		/proc/touchpanel/sweep_wake_enable
    			bit 0 = sweep2wake - on/off
    			bit 1 = disable haptic feedback for double tap to wake - on/off
    
    		/proc/touchpanel/sweep_wake_enable_implemented
    			shows which functions are implemented (see above bit fields)
    
    Signed-off-by: andip71 <andreasp@gmx.de>

--- drivers/staging/android/timed_output.c
+++ drivers/staging/android/timed_output.c
@@ -27,6 +27,13 @@
 
 static struct class *timed_output_class;
 static atomic_t device_count;
+int ignore_next_request = 0;
+
+
+void qpnp_hap_ignore_next_request(void)
+{
+	ignore_next_request = 1;
+}
 
 static ssize_t enable_show(struct device *dev, struct device_attribute *attr,
 			   char *buf)
@@ -48,7 +55,12 @@ static ssize_t enable_store(struct device *dev, struct device_attribute *attr,
 	if (rc != 0)
 		return -EINVAL;
 
-	tdev->enable(tdev, value);
+	// check if a gesture has been detected before, for which we should suppress
+	// the haptic feedback
+	if ((ignore_next_request) && (value != 0))
+		ignore_next_request = 0;
+	else
+		tdev->enable(tdev, value);
 
 	return size;
 }
