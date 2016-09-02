66012ec41989bfc554b36487c1cd7a44a97fb29a
andip71 <andreasp@gmx.de>
2016-09-02 10:17:45 +0200

    synaptics_driver_s3320: Fix sweep_wake_enabled sysfs not being written when screen is off
    - this was taken over from official write function, which appears to be a bug in official sources
    - check for ts=suspended removed for both sweep_wake_enabled and official write functions
    
    Signed-off-by: andip71 <andreasp@gmx.de>

--- drivers/input/touchscreen/synaptics_driver_s3320.c
+++ drivers/input/touchscreen/synaptics_driver_s3320.c
@@ -1733,7 +1733,8 @@ static ssize_t tp_gesture_write_func(struct file *file, const char __user *buffe
 	struct synaptics_ts_data *ts = ts_g;
 	if(!ts)
 		return count;
-	if( count > 3 || ts->is_suspended)
+	// if( count > 3 || ts->is_suspended)   AP: I believe the check if screen is off is a bug in official sources
+	if( count > 3)
 		return count;
 	if( copy_from_user(buf, buffer, count) ){
 		TPD_ERR(KERN_INFO "%s: read proc input error.\n", __func__);
@@ -1792,7 +1793,7 @@ static ssize_t tp_sweep_wake_write_func(struct file *file, const char __user *bu
 
 	if(!ts)
 		return count;
-	if( count > 2 || ts->is_suspended)
+	if( count > 2)
 		return count;
 	if( copy_from_user(buf, buffer, count) ){
 		TPD_ERR(KERN_INFO "%s: read proc input error.\n", __func__);
