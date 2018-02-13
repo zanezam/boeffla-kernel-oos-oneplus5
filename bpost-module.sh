82bb27d13b0775a26bfa119ec13664a531283d4c
ZaneZam <cyxman@yahoo.com>
2018-02-13 15:23:47 +0100

    compile: fix execution of post module script when using own compile script

--- bpost-module.sh
+++ bpost-module.sh
@@ -10,7 +10,7 @@
 echo -e ">>> post module handling\n"
 
 # retrieve path to modules folder
-MODULES_PATH = $1
+MODULES_PATH="$1"
 
 # do special handling
 mv $MODULES_PATH/wlan.ko $MODULES_PATH/qca_cld3_wlan.ko
